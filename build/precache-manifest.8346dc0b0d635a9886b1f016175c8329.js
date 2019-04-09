31028ae976293a3ed20ea987ca71f61472f00f85
achimonchi <achimonchi.reyhan@gmail.com>
2019-04-09 21:39:36 +0700

    react

---
+++ build/precache-manifest.8346dc0b0d635a9886b1f016175c8329.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "1316a231d7251c4a76d6e3ee24208136",
+    "url": "/portfolio/static/media/background.1316a231.svg"
+  },
+  {
+    "revision": "0209ac9d5254a39dbcfe",
+    "url": "/portfolio/static/css/main.6f2d3577.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "eb291c5dd6ea97b6ecb50578dc6ff25d",
+    "url": "/portfolio/static/media/_DSC0136-1.eb291c5d.jpg"
+  },
+  {
+    "revision": "29e42b845be1bd690c55",
+    "url": "/portfolio/static/js/2.3011e8df.chunk.js"
+  },
+  {
+    "revision": "a41ae6a6440f96da348c15b6583e5087",
+    "url": "/portfolio/static/media/ilshs.a41ae6a6.jpg"
+  },
+  {
+    "revision": "5cc31ea05a9005d97bf545f9411ab05c",
+    "url": "/portfolio/static/media/All-Design.5cc31ea0.png"
+  },
+  {
+    "revision": "0209ac9d5254a39dbcfe",
+    "url": "/portfolio/static/js/main.00edaef4.chunk.js"
+  },
+  {
+    "revision": "386e5c7ef119319560d870b5fba9631c",
+    "url": "/portfolio/static/media/isometrik01.386e5c7e.svg"
+  },
+  {
+    "revision": "8e0995a338d3304ed5132d2063dfa784",
+    "url": "/portfolio/static/media/ito.8e0995a3.jpg"
+  },
+  {
+    "revision": "99ee594479577e42fc068dd043090851",
+    "url": "/portfolio/static/media/_me.99ee5944.svg"
+  },
+  {
+    "revision": "06f4d5802a87553720e73f86308010b3",
+    "url": "/portfolio/static/media/bg.jpeg.06f4d580.svg"
+  },
+  {
+    "revision": "43a04c8393b0eba8fe60d595fd855fa1",
+    "url": "/portfolio/static/media/bg-skill.43a04c83.jpg"
+  },
+  {
+    "revision": "29e42b845be1bd690c55",
+    "url": "/portfolio/static/css/2.f4113aa4.chunk.css"
+  },
+  {
+    "revision": "64f1a3f330a6ade2159ecf573d5c9331",
+    "url": "/portfolio/index.html"
+  }
+];
